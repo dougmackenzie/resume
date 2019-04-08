528ab7650080ae5fe07a2f092f5ce58943b73b65
Doug MacKenzie <douglmackenzie@gmail.com>
2019-04-08 15:34:54 +1000

    Implement accordions for skills

---
+++ src/components/Accordion/Accordion.js
@@ -0,0 +1,95 @@
+import styled from "styled-components";
+
+const Accordion = styled.dl`
+  margin: 0;
+  box-shadow: 0 2px 3px rgba(10, 10, 10, 0.1), 0 0 0 1px rgba(10, 10, 10, 0.1);
+  background: #fff;
+`;
+
+const AccordionHeader = styled.dt``;
+
+const AccordionIcon = styled.div`
+  display: block;
+  width: 40px;
+  height: 40px;
+  margin-left: auto;
+  position: relative;
+  transition: all ease-in-out 0.2s;
+
+  &:after,
+  &:before {
+    background-color: #333;
+    content: "";
+    height: 3px;
+    position: absolute;
+    top: 10px;
+    transition: all ease-in-out 0.2s;
+    width: 30px;
+  }
+
+  &:before {
+    left: 1px;
+    transform: rotate(45deg) translate3d(8px, 22px, 0);
+    transform-origin: 100%;
+  }
+
+  &:after {
+    transform: rotate(-45deg) translate3d(-8px, 22px, 0);
+    right: 1px;
+    transform-origin: 0;
+  }
+`;
+
+const AccordionTitle = styled.div`
+  font-size: 18px;
+`;
+
+const AccordionButton = styled.button`
+  align-content: space-between;
+  align-items: center;
+  background-color: #fff;
+  border: 0;
+  border-radius: 0;
+  display: flex;
+  line-height: 1;
+  transition: all ease-in-out 0.2s;
+  width: 100%;
+  padding: 1.5rem;
+
+  &[aria-expanded="true"] {
+    ${AccordionIcon} {
+      &:before {
+        transform: rotate(45deg) translate3d(13px, 14px, 0);
+      }
+
+      &:after {
+        transform: rotate(-45deg) translate3d(-13px, 14px, 0);
+      }
+    }
+  }
+
+  // Removing "inner outline" for Firefox
+  &::-moz-focus-inner {
+    border: none;
+  }
+`;
+
+const AccordionPanel = styled.dd`
+  position: relative;
+  margin: 0;
+  transition: max-height ease-in-out 0.2s;
+`;
+
+const AccordionPanelInner = styled.div`
+  padding: 1rem 1.5rem;
+`;
+
+export default Accordion;
+export {
+  AccordionHeader,
+  AccordionButton,
+  AccordionIcon,
+  AccordionTitle,
+  AccordionPanel,
+  AccordionPanelInner
+};
--- src/pages/Skills/Skills.js
+++ src/pages/Skills/Skills.js
@@ -1,138 +1,163 @@
 import * as React from "react";
+
+import BadgerAccordion from "badger-accordion";
+import "badger-accordion/dist/badger-accordion.css";
+
 import Section, {
   SectionHeader,
   SectionTitle,
   SectionSubTitle
 } from "../../components/Section/Section";
-import Card, { CardContent, CardTitle } from "../../components/Card/Card";
 import Grid from "../../components/Grid/Grid";
 import Column from "../../components/Grid/Column";
+import Accordion, {
+  AccordionHeader,
+  AccordionButton,
+  AccordionIcon,
+  AccordionTitle,
+  AccordionPanel,
+  AccordionPanelInner
+} from "../../components/Accordion/Accordion";
 
-const Skills = props => (
-  <Section id={props.id} style={{ backgroundColor: "#f5f5f5" }}>
-    <SectionHeader>
-      <SectionTitle>Skills</SectionTitle>
-      <SectionSubTitle>What I can do</SectionSubTitle>
-    </SectionHeader>
+const mainSkills = [
+  {
+    title: "JavaScript",
+    description:
+      "I've been working with JavaScript for my entire career, however like many developers in my era- I didn't take the time to learn the language properly. In the past year I have been focusing on understanding the core of the language."
+  },
+  {
+    title: "React",
+    description:
+      "I was a bit late to the party with React, I only picked it up a year ago. However I've been working with it heavily since that point."
+  },
+  {
+    title: "UI Design",
+    description:
+      "Creating intuitive interfaces for users has long been a passion of mine.I am often called upon when there is a creative solution required for a user need or problem."
+  },
+  {
+    title: "CSS & Scoped Styling",
+    description:
+      "Dealing with CSS quirks has long been a speciality of mine. More recently I have been working with Styled Components for scoped styling."
+  }
+];
 
-    <h5>Specialties</h5>
-    <Grid>
-      <Column>
-        <Card>
-          <CardContent>
-            <CardTitle>JavaScript</CardTitle>
-            <p>
-              I've been working with JavaScript for my entire career, however
-              like many developers in my era- I didn't take the time to learn
-              the language properly. In the past year I have been focusing on
-              understanding the core of the language.
-            </p>
-          </CardContent>
-        </Card>
-      </Column>
+const additionalSkills = [
+  [
+    {
+      title: "TypeScript",
+      description:
+        "I worked with TypeScript for 6 months while integrating React atSchoolbox."
+    },
+    {
+      title: "Vue.js",
+      description:
+        "Vue was the first component based framework I learned. I created a couple of interfaces using Vue at Schoolbox."
+    },
+    {
+      title: "PHP",
+      description:
+        "I have worked with PHP my entire career. Though I specialise in front-end development - I am capable of working with PHP to get things done when needed."
+    },
+    {
+      title: "Laravel",
+      description: "I have built a couple of side projects with Laravel. "
+    }
+  ],
+  [
+    {
+      title: "Accessibility",
+      description:
+        "Accessibility is an important focus point when I create interfaces."
+    },
+    {
+      title: "Scrum",
+      description:
+        "During my time at Schoolbox I spent the last year managing Scrum Master responsibilites on top of my developer responsibilities. I have been part of three PD courses around Scrum and Agile methodologies and was in charge of improving many processes."
+    },
+    {
+      title: "Code Review",
+      description:
+        "Code review has been large part of my responsibilites over the last two and a half years. I pride myself in being able to find performance and practice improvements, whilst finding the balance in effort vs time."
+    },
 
-      <Column>
-        <Card>
-          <CardContent>
-            <CardTitle>React</CardTitle>
-            <p>
-              I was a bit late to the party with React, I only picked it up a
-              year ago. However I've been working with it heavily since that
-              point.
-            </p>
-          </CardContent>
-        </Card>
-      </Column>
+    {
+      title: "UX Research",
+      description:
+        "I have been a part of designing and conducting surveys, interviews, testing."
+    }
+  ]
+];
 
-      <Column>
-        <Card>
-          <CardContent>
-            <CardTitle>UI Design</CardTitle>
-            <p>
-              Creating intuitive interfaces for users has long been a passion of
-              mine. I am often called upon when there is a creative solution
-              required for a user need or problem.
-            </p>
-          </CardContent>
-        </Card>
-      </Column>
+const Skills = props => {
+  React.useEffect(() => {
+    const accordions = document.querySelectorAll("[data-accordion]");
 
-      <Column>
-        <Card>
-          <CardContent>
-            <CardTitle>CSS &amp; Scoped Styling</CardTitle>
-            <p>
-              Dealing with CSS quirks has long been a speciality of mine. More
-              recently I have been working with Styled Components for scoped
-              styling.
-            </p>
-          </CardContent>
-        </Card>
-      </Column>
-    </Grid>
+    Array.from(accordions).forEach(accordion => {
+      let accordionOptions = {};
 
-    <h5>Additional Skills</h5>
-    <Grid>
-      <Column>
-        <Card>
-          <CardContent>
-            <CardTitle>TypeScript</CardTitle>
-          </CardContent>
-        </Card>
-      </Column>
-      <Column>
-        <Card>
-          <CardContent>
-            <CardTitle>Accessibility</CardTitle>
-          </CardContent>
-        </Card>
-      </Column>
-      <Column>
-        <Card>
-          <CardContent>
-            <CardTitle>Vue.js</CardTitle>
-          </CardContent>
-        </Card>
-      </Column>
-      <Column>
-        <Card>
-          <CardContent>
-            <CardTitle>Scrum</CardTitle>
-          </CardContent>
-        </Card>
-      </Column>
-    </Grid>
-    <Grid>
-      <Column>
-        <Card>
-          <CardContent>
-            <CardTitle>jQuery</CardTitle>
-          </CardContent>
-        </Card>
-      </Column>
-      <Column>
-        <Card>
-          <CardContent>
-            <CardTitle>PHP</CardTitle>
-          </CardContent>
-        </Card>
-      </Column>
-      <Column>
-        <Card>
-          <CardContent>
-            <CardTitle>Laravel</CardTitle>
-          </CardContent>
-        </Card>
-      </Column>
-      <Column>
-        <Card>
-          <CardContent>
-            <CardTitle>SQL</CardTitle>
-          </CardContent>
-        </Card>
-      </Column>
-    </Grid>
-  </Section>
-);
+      if (accordion.hasAttribute("data-accordion-open")) {
+        accordionOptions.openHeadersOnLoad = [0];
+      }
+
+      new BadgerAccordion(accordion, accordionOptions);
+    });
+  }, []);
+
+  return (
+    <Section id={props.id} style={{ backgroundColor: "#f5f5f5" }}>
+      <SectionHeader>
+        <SectionTitle>Skills</SectionTitle>
+        <SectionSubTitle>What I can do</SectionSubTitle>
+      </SectionHeader>
+
+      <h5>Specialties</h5>
+
+      <Grid>
+        {mainSkills.map(mainSkill => (
+          <Column>
+            <Accordion data-accordion data-accordion-open>
+              <AccordionHeader>
+                <AccordionButton className="js-badger-accordion-header">
+                  <AccordionTitle>{mainSkill.title}</AccordionTitle>
+                  <AccordionIcon />
+                </AccordionButton>
+              </AccordionHeader>
+              <AccordionPanel className="badger-accordion__panel js-badger-accordion-panel">
+                <AccordionPanelInner className="js-badger-accordion-panel-inner">
+                  {mainSkill.description}
+                </AccordionPanelInner>
+              </AccordionPanel>
+            </Accordion>
+          </Column>
+        ))}
+      </Grid>
+
+      <h5>Additional Skills</h5>
+
+      {additionalSkills.map(additionalSkillsRow => (
+        <Grid>
+          {additionalSkillsRow.map(additionalSkill => (
+            <Column>
+              <Accordion data-accordion>
+                <AccordionHeader>
+                  <AccordionButton className="js-badger-accordion-header">
+                    <AccordionTitle>{additionalSkill.title}</AccordionTitle>
+                    <AccordionIcon />
+                  </AccordionButton>
+                </AccordionHeader>
+                <AccordionPanel className="badger-accordion__panel js-badger-accordion-panel">
+                  <AccordionPanelInner className="js-badger-accordion-panel-inner">
+                    {additionalSkill.description}
+                  </AccordionPanelInner>
+                </AccordionPanel>
+              </Accordion>
+            </Column>
+          ))}
+        </Grid>
+      ))}
+    </Section>
+  );
+};
 
 export default Skills;
